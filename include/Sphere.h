648d0ccce3248919e32c1e314d5148f4b3e2c396
guillaume-gomez <guillaume.gomez846@gmail.com>
2019-01-13 00:09:25 +0100

    add intersect metho

--- include/Sphere.h
+++ include/Sphere.h
@@ -1,12 +1,13 @@
 #ifndef SPHERE_H
 #define SPHERE_H
 
-#include "SceneObject.h";
+#include "SceneObject.h"
 
 class Sphere : public SceneObject
 {
     public:
         Sphere(Vector3 point = Vector3(), Vector3 color = Vector3(), float specular = 0.0f, float lambert = 0.0f, float ambiant = 0.0f, float radius = 0.0f);
+        float intersect(const Ray& ray) const;
         virtual ~Sphere();
     protected:
         float radius;
--- src/Sphere.cpp
+++ src/Sphere.cpp
@@ -1,9 +1,23 @@
 #include "Sphere.h"
+#include <math.h>
 
 Sphere::Sphere(Vector3 _point, Vector3 _color, float _specular, float _lambert, float _ambiant, float _radius)
 : SceneObject(_point, _color, _specular, _lambert, _ambiant), radius(_radius)
 {
     type = "Sphere";
+}
+
+float Sphere::intersect(const Ray& ray) const {
+    Vector3 eyeToCenter = point - ray.getOrigin();
+    Vector3 v = Vector3::crossProduct(eyeToCenter, ray.getDirection());
+    Vector3 eoDot = Vector3::crossProduct(eyeToCenter, eyeToCenter);
+    float lengthVEoDot = (eoDot + (v * v)).magnitude();
+    float discriminant = (radius * radius) - lengthVEoDot;
+    if(discriminant < 0) {
+        return 0;
+    } else {
+        return v.magnitude() - sqrt(discriminant);
+    }
 }
 
 Sphere::~Sphere()
