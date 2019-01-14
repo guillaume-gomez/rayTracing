3825732a6f1687fda95ed5fd6a6cd4f243f6e848
guillaume-gomez <guillaume.gomez846@gmail.com>
2019-01-15 00:40:25 +0100

    fix wrong parameter passed in function

--- include/Sphere.h
+++ include/Sphere.h
@@ -8,7 +8,7 @@ class Sphere : public SceneObject
     public:
         Sphere(Vector3 point = Vector3(), Vector3 color = Vector3(), float specular = 0.0f, float lambert = 0.0f, float ambiant = 0.0f, float radius = 0.0f);
         float intersect(const Ray& ray) const;
-        virtual Vector3 computeNormal(const Ray& pos) const;
+        virtual Vector3 computeNormal(const Vector3& pos) const;
         virtual ~Sphere();
     protected:
         float radius;
