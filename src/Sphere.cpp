90ddfbda73057e959bc3fee3862d40d1dd8d20bc
guillaume-gomez <guillaume.gomez846@gmail.com>
2019-01-15 00:40:03 +0100

    fix algorithm to detect colission between ray and sphere

--- src/Sphere.cpp
+++ src/Sphere.cpp
@@ -4,23 +4,24 @@
 Sphere::Sphere(Vector3 _point, Vector3 _color, float _specular, float _lambert, float _ambiant, float _radius)
 : SceneObject(_point, _color, _specular, _lambert, _ambiant), radius(_radius)
 {
+    // Point attribute means center here
     type = "Sphere";
 }
 
 float Sphere::intersect(const Ray& ray) const {
-    Vector3 eyeToCenter = point - ray.getOrigin();
-    Vector3 v = Vector3::crossProduct(eyeToCenter, ray.getDirection());
-    Vector3 eoDot = Vector3::crossProduct(eyeToCenter, eyeToCenter);
-    float lengthVEoDot = (eoDot + (v * v)).magnitude();
-    float discriminant = (radius * radius) - lengthVEoDot;
+    Vector3 rayOriginToCenter = point - ray.getOrigin();
+    float rayT = rayOriginToCenter * ray.getDirection(); // dot product
+    float rayOriginToCenterLength = rayOriginToCenter * rayOriginToCenter; // dot product
+    float discriminant = (radius * radius) - rayOriginToCenterLength + (rayT * rayT);
+
     if(discriminant < 0) {
         return 0;
     } else {
-        return v.magnitude() - sqrt(discriminant);
+        return rayOriginToCenterLength - sqrt(discriminant);
     }
 }
 
-Vector3 Sphere::computeNormal(const Ray& pos) const {
+Vector3 Sphere::computeNormal(const Vector3& pos) const {
     return (pos - point).unitVector();
 }
 
