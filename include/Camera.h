8244b1e7df986291e9a5400185ae788801354830
guillaume-gomez <guillaume.gomez846@gmail.com>
2019-01-18 00:01:55 +0100

    add new attributes + accesor in camera

--- include/Camera.h
+++ include/Camera.h
@@ -14,9 +14,15 @@ class Camera
         float getFov() const { return fov; };
         const Vector3& getPoint() const { return point; };
         const Vector3& getDirection() const { return direction; };
+        const Vector3& getForward() const { return forward; };
+        const Vector3& getUp() const { return up; };
+        const Vector3& getRight() const { return right; };
      private:
         Vector3 point;
         Vector3 direction;
+        Vector3 forward;
+        Vector3 up;
+        Vector3 right;
         float fov;
 };
 
