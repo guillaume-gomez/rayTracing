184500de19b18a1979af36fb09ba279989b8f742
guillaume-gomez <guillaume.gomez846@gmail.com>
2019-01-18 00:02:07 +0100

    add plane

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 #include "Scene.h"
 #include "Sphere.h"
+#include "Plane.h"
 
 using namespace std;
 
@@ -17,10 +18,12 @@ int main()
     Sphere s1 = Sphere(Vector3(0.0f, 3.5f, -3.0f), Vector3(155.0f, 200.0f, 155.0f), 0.2f, 0.7f, 0.1f, 3.0f);
     Sphere s2 = Sphere(Vector3(-4.0f, 2.0f, -1.0f), Vector3(155.0f, 155.0f, 155.0f), 0.1f, 0.9f, 0.0f, 0.2f);
     Sphere s3 = Sphere(Vector3(-4.0f, 3.0f, -1.0f), Vector3(255.0f, 255.0f, 255.0f), 0.2f, 0.7f, 0.1f, 0.1f);
-
+    Plane p = Plane(Vector3(0.0f, 0.0f, -175.0f), Vector3(0.0f, 10.0f, 155.0f), 0.2f, 0.7f, 0.1f, Vector3(0.0f, 0.0f, -1.0f));
     Scene scene = Scene(width, height, camera);
     scene.addLight(light);
+    scene.addObject(&p);
     scene.addObject(&s1);
+
     scene.addObject(&s2);
     scene.addObject(&s3);
 
