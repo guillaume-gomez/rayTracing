6713d91dd1c3abc861d3ab0d7897f89feb995e04
guillaume-gomez <guillaume.gomez846@gmail.com>
2019-01-19 00:12:30 +0100

    add attribute in camera object

--- include/Camera.h
+++ include/Camera.h
@@ -6,24 +6,28 @@
 class Camera
 {
     public:
-        Camera(Vector3 point = Vector3(), Vector3 direction = Vector3(), float fov = 45.0f);
+        Camera(Vector3 point = Vector3(), Vector3 lookAt = Vector3(), float fov = 45.0f);
         Camera(const Camera& copy);
         Camera& operator=(const Camera& source);
         virtual ~Camera();
         Vector3 eyeVector();
         float getFov() const { return fov; };
         const Vector3& getPoint() const { return point; };
-        const Vector3& getDirection() const { return direction; };
+        const Vector3& getLookAt() const { return lookAt; };
         const Vector3& getForward() const { return forward; };
         const Vector3& getUp() const { return up; };
         const Vector3& getRight() const { return right; };
      private:
         Vector3 point;
-        Vector3 direction;
-        Vector3 forward;
+        Vector3 lookAt;
         Vector3 up;
         Vector3 right;
+        Vector3 viewPlaneLeft;
         float fov;
+        float viewPlaneWidth;
+        float viewPlaneHeight;
+        float viewPlaneDist;
+        Vector3 forward;
 };
 
 #endif // CAMERA_H
--- src/Camera.cpp
+++ src/Camera.cpp
@@ -1,27 +1,36 @@
 #include "Camera.h"
 
-Camera::Camera(Vector3 _point, Vector3 _direction, float _fov)
-: point(_point), direction(_direction), fov(_fov)
+Camera::Camera(Vector3 _point, Vector3 _lookAt, float _fov)
+: point(_point), lookAt(_lookAt), fov(_fov)
 {
-    //ctor
+    viewPlaneDist     = 1.0f;
+    viewPlaneHeight   = 0.35f;
+    viewPlaneWidth    = 0.5f;
+
+
+    Vector3 globalUp = (0.0f, 1.0f, 0.0f);
+    right = globalUp * eyeVector();
+    up = right * eyeVector();
+
+    viewPlaneLeft = point + ((eyeVector() * viewPlaneDist) + (up * (viewPlaneHeight / 2.0f)))-(right * ( viewPlaneWidth / 2.0f));
+
 }
 
 Camera::Camera(const Camera& original)
-: point(original.point), direction(original.direction), fov(original.fov)
+: point(original.point), lookAt(original.lookAt), fov(original.fov), up(original.up)
 {
-
 }
 
 Camera& Camera::operator=(const Camera& source) {
     point = source.point;
-    direction = source.direction;
+    lookAt = source.lookAt;
     fov = source.fov;
     return *this;
 }
 
 Vector3 Camera::eyeVector() {
-    Vector3 vec = direction - point;
-    return vec.unitVector();
+    forward = lookAt - point;
+    return forward.unitVector();
 }
 
 Camera::~Camera()
