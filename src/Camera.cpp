a8a7c88ce8c0e821d6bc249ac4644b54a069cf27
guillaume-gomez <guillaume.gomez846@gmail.com>
2019-01-21 00:01:54 +0100

    add comment and set contant

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -2,15 +2,15 @@
 
 #include <iostream>
 
+Vector3 globalUp = Vector3(0.0f, 1.0f, 0.0f);
+
 Camera::Camera(Vector3 _point, Vector3 _lookAt, float _fov)
 : point(_point), lookAt(_lookAt), fov(_fov)
 {
     viewPlaneDist     = 1.0f;
-    viewPlaneHeight   = 0.5f;
+    viewPlaneHeight   = 0.35f;
     viewPlaneWidth    = 0.5f;
 
-
-    Vector3 globalUp = (0.0f, 1.0f, 0.0f);
     right = Vector3::crossProduct(globalUp, eyeVector()).unitVector();
     up = Vector3::crossProduct(right, eyeVector()).unitVector();
 
@@ -19,13 +19,8 @@ Camera::Camera(Vector3 _point, Vector3 _lookAt, float _fov)
 }
 
 Camera::Camera(const Camera& original)
-: point(original.point), lookAt(original.lookAt), fov(original.fov), up(original.up)
+: point(original.point), lookAt(original.lookAt), fov(original.fov), up(original.up), viewPlaneDist(original.viewPlaneDist), viewPlaneHeight(original.viewPlaneHeight), viewPlaneWidth(original.viewPlaneWidth)
 {
-    viewPlaneDist     = 1.0f;
-    viewPlaneHeight   = 0.35f;
-    viewPlaneWidth    = 0.5f;
-
-    Vector3 globalUp = Vector3(0.0f, 1.0f, 0.0f);
     right = Vector3::crossProduct(globalUp, eyeVector()).unitVector();
     up = Vector3::crossProduct(right, eyeVector()).unitVector();
 
@@ -43,6 +38,33 @@ Vector3 Camera::calcDirVec(float x, float y, int xRes, int yRes) {
 
     Vector3 raydir = (viewPlaneUpLeft + right * xIndent * x - up * yIndent * y) - point;
     return raydir.unitVector();
+
+    //TODO
+    //  Vector3 eyeVector = camera.eyeVector();
+    // Vector3 up = Vector3(0.0f, 1.0f, 0.0f);
+
+    // Vector3 vpRight = (Vector3::crossProduct(eyeVector, up)).unitVector();
+    // Vector3 vpUp = (Vector3::crossProduct(vpRight, eyeVector)).unitVector();
+
+    // float fovRadians = M_PI * (camera.getFov() / 2.0f) / 180.f;
+    // float heightWidthRatio = height / width;
+    // float halfWidth = tan(fovRadians);
+    // float halfHeight = heightWidthRatio * halfWidth;
+    // float camerawidth = halfWidth * 2.0f;
+    // float cameraheight = halfHeight * 2.0f;
+    // float pixelWidth = camerawidth / (width - 1.0f);
+    // float pixelHeight = cameraheight / (height - 1.0f);
+
+    //  // turn the raw pixel `x` and `y` values into values from -1 to 1
+    //         // and use these values to scale the facing-right and facing-up
+    //         // vectors so that we generate versions of the `eyeVector` that are
+    //         // skewed in each necessary direction.
+    //         Vector3 xcomp = vpRight * ( (x * pixelWidth) - halfWidth);
+    //         Vector3 ycomp = vpUp    * ( (y * pixelHeight) - halfHeight);
+
+
+    //         Vector3 rayDirection = (eyeVector + xcomp + ycomp).unitVector();
+           
 }
 
 Camera& Camera::operator=(const Camera& source) {
