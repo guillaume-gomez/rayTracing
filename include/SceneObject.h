96e10a1bdcd109fc42a432465e3bffb31ca039d7
guillaume-gomez <guillaume.gomez846@gmail.com>
2019-01-23 00:12:12 +0100

    add accessort to get type of each SceneObject

--- include/SceneObject.h
+++ include/SceneObject.h
@@ -11,12 +11,14 @@ class SceneObject
     public:
         SceneObject(Vector3 point = Vector3(), Vector3 color = Vector3(), float specular = 0.0f, float lambert = 0.0f, float ambiant = 0.0f);
         virtual float intersect(const Ray& ray) const = 0;
+        virtual Vector3 computeNormal(const Vector3& pos) const = 0;
         virtual ~SceneObject();
         const Vector3 getColor() const { return color; };
         const Vector3 getPoint() const { return point; };
         const float getSpecular() const { return specular; };
         const float getLambert() const { return lambert; };
         const float getAmbiant() const { return ambiant; };
+        const std::string getType() const { return type; };
     protected:
         std::string type;
         Vector3 point;
