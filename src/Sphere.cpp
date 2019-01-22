95208f622ed2d00b01cbbd916d47bf86dd60068d
guillaume-gomez <guillaume.gomez846@gmail.com>
2019-01-23 00:14:57 +0100

    change detection algorithm

--- src/Sphere.cpp
+++ src/Sphere.cpp
@@ -1,6 +1,9 @@
 #include "Sphere.h"
 #include <math.h>
 
+#include <limits>
+#include <iostream>
+
 Sphere::Sphere(Vector3 _point, Vector3 _color, float _specular, float _lambert, float _ambiant, float _radius)
 : SceneObject(_point, _color, _specular, _lambert, _ambiant), radius(_radius)
 {
@@ -9,16 +12,33 @@ Sphere::Sphere(Vector3 _point, Vector3 _color, float _specular, float _lambert, 
 }
 
 float Sphere::intersect(const Ray& ray) const {
-    Vector3 rayOriginToCenter = point - ray.getOrigin();
-    float rayT = rayOriginToCenter * ray.getDirection(); // dot product
-    float rayOriginToCenterLength = rayOriginToCenter * rayOriginToCenter; // dot product
-    float discriminant = (radius * radius) - rayOriginToCenterLength + (rayT * rayT);
-
-    if(discriminant < 0) {
-        return 0;
-    } else {
-        return rayOriginToCenterLength - sqrt(discriminant);
+    Vector3 L = point - ray.getOrigin();
+    float radius2 = radius * radius;
+    float tca = L * ray.getDirection();
+    if (tca < 0) {
+        return std::numeric_limits<float>::infinity();
     }
+    float d2 = (L * L) - (tca * tca);
+    if (d2 > radius2) {
+        return std::numeric_limits<float>::infinity();
+    }
+    float thc = sqrt(radius2 - d2);
+    float t0 = tca - thc;
+    float t1 = tca + thc;
+    if (t0 > t1) {
+        std::swap(t0, t1);
+    }
+
+    if (t0 < 0) {
+        t0 = t1; // if t0 is negative, let's use t1 instead
+        if (t0 < 0) {
+            return std::numeric_limits<float>::infinity(); // both t0 and t1 are negative
+        }
+    }
+
+    float t = t0;
+
+    return t;
 }
 
 Vector3 Sphere::computeNormal(const Vector3& pos) const {
