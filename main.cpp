db07109a6272c0572baa66fd6590ba1a3a9f1331
guillaume-gomez <guillaume.gomez846@gmail.com>
2019-01-24 00:50:48 +0100

    update scene

--- main.cpp
+++ main.cpp
@@ -27,10 +27,10 @@ int main()
     Camera camera = Camera(Vector3(0.0f, 0.0f, -3.0f), Vector3(0.0f, 0.0f, 10.0f));
     Light light = Light(Vector3(-30.0f, -10.0f, -20.0f));
 
-    Sphere s1 = Sphere(Vector3(0.0f, 0.0f, 10.0f), Vector3(155.0f, 200.0f, 155.0f), 0.2f, 0.7f, 0.1f, 2.0f);
-    Sphere s2 = Sphere(Vector3(-2.0f, 1.0f, 9.0f), Vector3(155.0f, 155.0f, 155.0f), 0.1f, 0.9f, 0.0f, 0.5f);
-    Sphere s3 = Sphere(Vector3(-1.5f, 1.0f, 8.0f), Vector3(10.0f, 255.0f, 255.0f), 0.2f, 0.7f, 0.1f, 0.4f);
-    Plane p = Plane(Vector3(0.0f, 0.0f, 150.0f), randomVector(), 0.2f, 0.7f, 0.1f, Vector3(0.0f, 0.0f, 1.0f));
+    Sphere s1 = Sphere(Vector3(0.0f, 0.0f, 11.0f), Vector3(155.0f, 200.0f, 155.0f), 0.2f, 0.7f, 0.1f, 1.0f);
+    Sphere s2 = Sphere(Vector3(-2.0f, 1.0f, 9.0f), Vector3(155.0f, 155.0f, 155.0f), 0.1f, 0.9f, 0.0f, 1.0f);
+    Sphere s3 = Sphere(Vector3(-1.5f, 2.0f, 7.0f), Vector3(10.0f, 255.0f, 255.0f), 0.2f, 0.7f, 0.1f, 1.0f);
+    Plane p = Plane(Vector3(0.0f, 0.0f, 13.0f), randomVector(), 0.2f, 0.7f, 0.1f, Vector3(0.0f, 0.0f, 1.0f));
     Scene scene = Scene(width, height, camera);
     scene.addLight(light);
     scene.addObject(&p);
@@ -41,7 +41,7 @@ int main()
 
 
     std::vector<unsigned char> image = scene.render();
-    std::string filename = "output.png";
+    std::string filename = "output1.png";
     unsigned error = lodepng::encode(filename, image, width, height);
     if(error) std::cout << "encoder error " << error << ": "<< lodepng_error_text(error) << std::endl;
 
