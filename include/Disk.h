dc8ccc79f8a26753b8a2e59c40c60cbefcd49190
guillaume-gomez <guillaume.gomez846@gmail.com>
2019-01-26 12:38:43 +0100

    add new object

---
+++ include/Disk.h
@@ -0,0 +1,19 @@
+#ifndef DISK_H
+#define DISK_H
+
+#include <Plane.h>
+
+
+class Disk : public Plane
+{
+    public:
+        Disk(Vector3 point = Vector3(), Vector3 color = Vector3(), float specular = 0.0f, float lambert = 0.0f, float ambiant = 0.0f, Vector3 normal = Vector3(), float radius = 0.0f);
+        Disk(const Disk& original);
+        virtual float intersect(const Ray& ray) const;
+        virtual Vector3 computeNormal(const Vector3& pos) const;
+        virtual ~Disk();
+    protected:
+        float radius;
+};
+
+#endif // DISK_H
---
+++ src/Disk.cpp
@@ -0,0 +1,45 @@
+#include "Disk.h"
+
+#include <limits>
+
+Disk::Disk(Vector3 _point, Vector3 _color, float _specular, float _lambert, float _ambiant, Vector3 _normal, float _radius)
+: Plane(_point, _color, _specular, _lambert, _ambiant, _normal), radius(_radius)
+{
+    //ctor
+    type = "Disk";
+}
+
+Disk::Disk(const Disk& original)
+: Plane(original), radius(original.radius)
+{
+    type = "Disk";
+}
+
+float Disk::intersect(const Ray& ray) const {
+
+    float t = Plane::intersect(ray);
+    float radius2 = radius * radius;
+
+    if(t < std::numeric_limits<float>::infinity() ) {
+        //check if intersection is in circle
+        Vector3 IntersectionPlane = ray.getOrigin() + ray.getDirection() * t;
+        Vector3 distance = IntersectionPlane - point;
+        float d2 = distance * distance;
+        if(d2 <= radius2) {
+            return t;
+        } else {
+            return std::numeric_limits<float>::infinity();
+        }
+    }
+    return t;
+}
+
+Vector3 Disk::computeNormal(const Vector3& pos) const {
+    return 0.0f;
+}
+
+
+Disk::~Disk()
+{
+    //dtor
+}
