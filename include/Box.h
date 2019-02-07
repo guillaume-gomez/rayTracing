130e717059bc695ac40c04c1f6d6b1734f0d34e1
guillaume-gomez <guillaume.gomez846@gmail.com>
2019-02-08 00:02:22 +0100

    add attribute and inheritance

--- include/Box.h
+++ include/Box.h
@@ -1,14 +1,19 @@
 #ifndef Box_H
 #define Box_H
 
+#include "SceneObject.h"
 
-class Box
+class Box : public SceneObject
 {
     public:
-        Box();
+        Box(Vector3 point = Vector3(), Vector3 color = Vector3(), float specular = 0.0f, float lambert = 0.0f, float ambiant = 0.0f, Vector3 vmin, Vector3 vmax);
+        Box(const Box& original);
         virtual ~Box();
+        float intersect(const Ray& ray) const;
+        virtual Vector3 computeNormal(const Vector3& pos) const;
     protected:
-    private:
+        Vector3 vmin;
+        Vector3 vmax;
 };
 
 #endif // Box_H
--- src/Box.cpp
+++ src/Box.cpp
@@ -1,8 +1,24 @@
 #include "Box.h"
 
-Box::Box()
+
+Box::Box(Vector3 _point, Vector3 _color, float _specular, float _lambert, float _ambiant, Vector3 _vmin, Vector3 _vmax)
+: point(_point), color(_color), specular(_specular), lambert(_lambert), ambiant(_ambiant), vmin(_vmin), vmax(_vmax), type("Box")
 {
     //ctor
+}
+
+Box::Box(const Box& original)
+: point(original.point), color(original.color), specular(original.specular), lambert(original.lambert), ambiant(original.ambiant), vmin(original.vmin), vmax(original.vmax), type("Box")
+{
+
+}
+
+float Box::intersect(const Ray& ray) const {
+    return 0.0f;
+}
+
+Vector3 Box::computeNormal(const Vector3& pos) const {
+    return Vector3(0.0f, 0.0f, 0.0f);
 }
 
 Box::~Box()
