0514b74a72bede4cfdfdef4c8af474497c6df072
guillaume-gomez <guillaume.gomez846@gmail.com>
2019-03-14 00:16:20 +0100

    add inverse ray to avoid to compute in each box

--- src/Ray.cpp
+++ src/Ray.cpp
@@ -3,24 +3,31 @@
 Ray::Ray(const Vector3& _origin, const Vector3& _direction)
 : origin(_origin), direction(_direction)
 {
-    inverseDirection = direction.inverse();
-    sign[0] = (inverseDirection.x < 0);
-    sign[1] = (inverseDirection.y < 0);
-    sign[2] = (inverseDirection.z < 0);
+    computeInverseDirection();
 }
 
 Ray::Ray(const Ray& original)
 : origin(original.origin), direction(original.direction), inverseDirection(original.inverseDirection)
 {
-    sign[0] = (inverseDirection.x < 0);
-    sign[1] = (inverseDirection.y < 0);
-    sign[2] = (inverseDirection.z < 0);
+    computeInverseDirection();
 }
 
 Ray& Ray::operator=(const Ray& source) {
     origin = source.origin;
     direction = source.direction;
     return *this;
+}
+
+void Ray::setDirection(Vector3 _direction) { 
+    direction = _direction;
+    computeInverseDirection();
+};
+
+void Ray::computeInverseDirection() {
+    inverseDirection = 1 / direction;
+    sign[0] = (inverseDirection.x < 0);
+    sign[1] = (inverseDirection.y < 0);
+    sign[2] = (inverseDirection.z < 0);
 }
 
 Ray::~Ray()
