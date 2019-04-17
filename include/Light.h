c809d4864ed43d86dbae9ac46758f72b085e6080
guillaume-gomez <guillaume.gomez846@gmail.com>
2019-04-18 01:57:01 +0200

    add direction in light

--- include/Light.h
+++ include/Light.h
@@ -5,11 +5,13 @@
 class Light
 {
     public:
-        Light(const Vector3 position);
+        Light(const Vector3 position, const Vector3 color = Vector3(255.f, 255.f, 255.f));
         virtual ~Light();
         Vector3 getPosition() const { return position; };
+        Vector3 getColor() const { return color; };
     private:
         Vector3 position;
+        Vector3 color;
 };
 
 #endif // LIGHT_H
--- src/Light.cpp
+++ src/Light.cpp
@@ -1,8 +1,8 @@
 #include "Vector3.h"
 #include "Light.h"
 
-Light::Light(const Vector3 _position)
-: position(_position)
+Light::Light(const Vector3 _position, const Vector3 _color)
+: position(_position), color(_color)
 {
     //ctor
 }
