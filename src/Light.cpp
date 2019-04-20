7bae031e1d545a1d2520eff9f5e7109e267a7e5b
guillaume-gomez <guillaume.gomez846@gmail.com>
2019-04-21 00:25:03 +0200

    add intensity in Light

--- src/Light.cpp
+++ src/Light.cpp
@@ -1,8 +1,8 @@
 #include "Vector3.h"
 #include "Light.h"
 
-Light::Light(const Vector3 _position, const Vector3 _color)
-: position(_position), color(_color)
+Light::Light(const Vector3 _position, const Vector3 _color, float _intensity)
+: position(_position), color(_color), intensity(_intensity)
 {
     //ctor
 }
