1747b3dee1e9e4b7a5a17d26682f0aa9b34309c5
guillaume-gomez <guillaume.gomez846@gmail.com>
2019-04-20 02:22:47 +0200

    move part of the code to scene

--- src/Window.cpp
+++ src/Window.cpp
@@ -1,14 +1,79 @@
 #include "Window.h"
 
-Window::Window(Scene& scene)
+float OFFSET = 0.01f;
+
+Window::Window(Scene& _scene):
+scene(_scene)
 {
     width = scene.getWidth();
     height = scene.getHeight();
-    sf::RenderWindow window(sf::VideoMode(width, height), "Ray Tracing View");
-    sf::Uint8 *pixels = new sf::Uint8[width * height * 4];
+    window = new sf::RenderWindow(sf::VideoMode(width, height), "Ray Tracing View");
+    pixels = new sf::Uint8[width * height * 4];
+    texture = new sf::Texture();
+    texture->create(width, height);
+    sprite = new sf::Sprite(*texture);
 }
+
+void Window::updateBuffer() {
+    std::vector<unsigned char> image = scene.render();
+    //if (scene.IsneededUpdate()) {
+        //std::cout << "rerender" << std::endl;
+        for(register int i = 0; i < image.size(); i += 4) {
+            pixels[i] = image[i];
+            pixels[i + 1] = image[i + 1];
+            pixels[i + 2] = image[i + 2];
+            pixels[i + 3] = image[i + 3];
+        }
+        texture->update(pixels);
+    //}
+}
+
+
+void Window::display() {
+    while (window->isOpen())
+    {
+        sf::Event event;
+        while (window->pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed) {
+                window->close();
+            }
+
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+                scene.getCamera().move(Vector3(-OFFSET, 0.0f, 0.0f));
+            }
+
+
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+                scene.getCamera().move(Vector3(OFFSET, 0.0f, 0.0f));
+            }
+
+
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+                scene.getCamera().move(Vector3(0.f, -OFFSET, 0.0f));
+            }
+
+
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+                scene.getCamera().move(Vector3(0.f, OFFSET, 0.0f));
+            }
+        }
+        updateBuffer();
+        render();
+    }
+}
+
+void Window::render() {
+    window->clear();
+    window->draw(*sprite);
+    window->display();
+}
+
 
 Window::~Window()
 {
-    //dtor
+    delete sprite;
+    delete texture;
+    delete pixels;
+    delete window;
 }
