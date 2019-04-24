bfed8d3cf5ab1c545a53bc76e76bef6d205e5146
guillaume-gomez <guillaume.gomez846@gmail.com>
2019-04-25 00:46:00 +0200

    add attributes in material

--- include/Material.h
+++ include/Material.h
@@ -1,16 +1,33 @@
 #ifndef MATERIAL_H
 #define MATERIAL_H
 
+#include "Color.h"
 
 class Material
 {
     public:
-        Material();
+        Material( Color specularColor = Color(0.0f, 0.0f, 0.0f),
+                  Color diffuseColor = Color(0.0f, 0.0f, 0.0f),
+                  Color ambientColor = Color(0.0f, 0.0f, 0.0f),
+                  Color selfIllumColor = Color(0.0f, 0.0f, 0.0f),
+                  float shininess = 0.0f,
+                  float shinestrength = 0.0f,
+                  float transmittivity = 0.0f,
+                  float reflectivity = 0.0f,
+                  std::string name = "undefined type"
+                );
         virtual ~Material();
 
     protected:
-
-    private:
+        Color  specularColor;
+        Color  diffuseColor;
+        Color  ambientColor;
+        Color  selfIllumColor;
+        float  shininess;
+        float  shinestrength;
+        float  transmittivity;
+        float  reflectivity;
+        std::string name;
 };
 
 #endif // MATERIAL_H
--- src/Material.cpp
+++ src/Material.cpp
@@ -1,6 +1,24 @@
 #include "Material.h"
 
-Material::Material()
+Material::Material( Color _specularColor,
+                    Color _diffuseColor,
+                    Color _ambientColor,
+                    Color _selfIllumColor,
+                    float _shininess,
+                    float _shinestrength,
+                    float _transmittivity,
+                    float _reflectivity,
+                    std::string _name
+                )
+: specularColor(_specularColor),
+  diffuseColor(_diffuseColor),
+  ambientColor(_ambientColor),
+  selfIllumColor(_selfIllumColor),
+  shininess(_shininess),
+  shinestrength(_shinestrength),
+  transmittivity(_transmittivity),
+  reflectivity(_reflectivity),
+  name(_name)
 {
     //ctor
 }
